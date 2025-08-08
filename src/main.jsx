b0ed6921e3d4f170989f989c34eb0f657cf3c7a1
Luna-bf <lburattifayolle@gmail.com>
2025-08-08 11:49:39 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import SyntaxeJsx from './1-Bases/SyntaxeJsx.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <SyntaxeJsx />
+  </StrictMode>,
+)
